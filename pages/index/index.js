1376570c6057881b80b957e7a5a919e554a4bab0
Asgardusk <13979215061@163.com>
2018-11-14 20:03:23 +0800

    主页，框框都搞定了，等待样式修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,45 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    movies: [{
+        url: '../../src/wash-foot01.jpg'
+      },
+      {
+        url: '../../src/wash-foot02.jpg'
+      },
+      {
+        url: '../../src/wash-foot03.jpg'
+      },
+      {
+        url: '../../src/wash-foot04.jpg'
+      }
+    ],
+    current: '1',
+    tab1: true
+  },
+  handleChange({ detail }) {
+    var index = detail.key
+    console.log(index)
+    this.setData({
+      current: detail.key
+    });
+    if (index == 1) {
+      this.setData({
+        tab1: true,
+        tab2: false
+      })
+    } else if (index == 2) {
+      this.setData({
+        tab1: false,
+        tab2: true
+      })
+    }
+  },
+
+  handleChangeScroll({ detail }) {
+    this.setData({
+      current_scroll: detail.key
+    });
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +50,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,12 +78,16 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  swipclick: function(e) {
+    //点击图片触发事件
+    console.log(this.data.imageUrls[this.data.current]);
   }
+  // getUserInfo: function(e) {
+  //   console.log(e)
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // }
 })
