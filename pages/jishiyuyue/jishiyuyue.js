3f0687f6b1cfdea8ca614a198f2ce09d0e3f4660
yun_cn <zzwyxl@163.com>
2018-11-20 12:43:38 +0800

    预约流程加上openid

--- pages/jishiyuyue/jishiyuyue.js
+++ pages/jishiyuyue/jishiyuyue.js
@@ -1,18 +1,19 @@
 // pages/jishiyuyue/jishiyuyue.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    openid:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({openid:app.globalData.openid})
   },
 
   /**
--- pages/tea/tea.js
+++ pages/tea/tea.js
@@ -1,18 +1,20 @@
 // pages/recharge/recharge.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    url:''
+    url:'',
+    openid:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({openid:app.globalData.openid})
   },
 
   /**
